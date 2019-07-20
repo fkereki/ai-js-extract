39eca7e31e2053e4c516ee4bbd5dabb693f1fb2f
Federico Kereki <fkereki@gmail.com>
2019-07-20 20:56:27 -0300

    Refactored jsDoc testing

--- index.js
+++ index.js
@@ -15,26 +15,19 @@ const ast = parser.parse(code, {
 fs.writeFileSync("./output.js", "const a = " + JSON.stringify(ast));
 
 const inspectTree = tree => {
+    const hasJSDoc = obj =>
+        obj.leadingComments && obj.leadingComments.length && obj.leadingComments[0].value.startsWith("*");
+
     tree &&
         tree.body &&
         tree.body.forEach(n => {
-            if (
-                n.type === "FunctionDeclaration" &&
-                n.leadingComments &&
-                n.leadingComments.length &&
-                n.leadingComments[0].value.startsWith("*")
-            ) {
+            if (n.type === "FunctionDeclaration" && hasJSDoc(n)) {
                 console.log("Found function\n", n.id.name);
                 console.log("with comments\n", n.leadingComments[0].value);
                 console.log("source\n", code.substring(n.start, n.end + 1));
             }
 
-            if (
-                n.type === "ClassMethod" &&
-                n.leadingComments &&
-                n.leadingComments.length &&
-                n.leadingComments[0].value.startsWith("*")
-            ) {
+            if (n.type === "ClassMethod" && hasJSDoc(n)) {
                 console.log("Found method\n", n.key.name);
                 console.log("with comments\n", n.leadingComments[0].value);
                 console.log("source\n", code.substring(n.start, n.end + 1));
