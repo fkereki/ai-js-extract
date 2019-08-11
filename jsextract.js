63e3c0ce5f09982bd91039244826ea22619f6f8e
Federico Kereki <fkereki@gmail.com>
2019-08-11 18:27:25 -0300

    Complete code

--- jsextract.js
+++ jsextract.js
@@ -2,12 +2,33 @@ const parser = require("@babel/parser");
 const fs = require("fs");
 const csv = require("fast-csv");
 
-const FILE_TO_PROCESS = "data_short";
+const FILE_TO_PROCESS = process.argv[2] || "./samples_js/data_short.csv";
+const FILE_TO_GENERATE = FILE_TO_PROCESS.replace(/\.csv$/i, "_output$&");
 
-const csvStream = csv.format({ headers: ["path", "name", "doc", "code"], quoteColumns: true });
-csvStream.pipe(fs.createWriteStream(`./samples_js/${FILE_TO_PROCESS}_output.csv`));
+const parserOptions = outputTokens => ({
+    sourceType: "unambiguous",
+    strictMode: false,
+    tokens: outputTokens, // true: emit all tokens instead of the AST
+    allowImportExportEverywhere: true,
+    allowUndeclaredExports: true,
+    plugins: [
+        "typescript",
+        "jsx",
+        "asyncGenerators",
+        "classProperties",
+        "decorators-legacy",
+        "dynamicImport",
+        "exportDefaultFrom"
+    ]
+});
 
-fs.createReadStream(`./samples_js/${FILE_TO_PROCESS}.csv`)
+const outputStream = csv.format({
+    headers: ["repo_path", "name", "start", "raw_code", "code", "doc", "tokens"],
+    quoteColumns: true
+});
+outputStream.pipe(fs.createWriteStream(FILE_TO_GENERATE));
+
+fs.createReadStream(FILE_TO_PROCESS)
     .pipe(csv.parse({ headers: ["repopath", "repocode"] }))
     .on("data", row => processCode(row.repopath, row.repocode));
 
@@ -18,26 +39,36 @@ function processCode(path, code) {
 
         if (tree && tree.body) {
             tree.body.forEach(n => {
-                if (n.type === "FunctionDeclaration" && hasJSDoc(n)) {
-                    csvStream.write([path, n.id.name, n.leadingComments[0].value, code.substring(n.start, n.end + 1)]);
+                if ((n.type === "FunctionDeclaration" || n.type === "ClassMethod") && hasJSDoc(n)) {
+                    const tokens = parser
+                        .parse(code.substring(n.start, n.end + 1), parserOptions(true))
+                        .tokens.map(x => x.value)
+                        .filter(Boolean)
+                        .join(" ");
+
+                    outputStream.write([
+                        path, // repo_path
+                        n.type === "FunctionDeclaration" ? n.id.name : n.key.name, // name
+                        n.leadingComments[0].start, // start
+                        code.substring(n.leadingComments[0].start, n.end + 1), // raw_code (inc doc)
+                        code.substring(n.start, n.end + 1), // code (without doc)
+                        code.substring(
+                            n.leadingComments[0].start,
+                            n.leadingComments[n.leadingComments.length - 1].end + 1
+                        ), //doc
+                        tokens // tokens
+                    ]);
                 }
-                if (n.type === "ClassMethod" && hasJSDoc(n)) {
-                    csvStream.write([path, n.key.name, n.leadingComments[0].value, code.substring(n.start, n.end + 1)]);
-                }
+
                 inspectTree(n.body);
             });
         }
     }
 
     try {
-        const ast = parser.parse(code, {
-            sourceType: "unambiguous",
-            strictMode: true,
-            tokens: false, // true: emit all tokens instead of the AST
-            plugins: []
-        });
+        const ast = parser.parse(code, parserOptions(false));
         inspectTree(ast.program);
     } catch (e) {
-        // nothing, now
+        console.log("EXCEPTION!", e, "<**************************>", code);
     }
 }
